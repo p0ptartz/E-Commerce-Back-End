43980439871420179c21d923ca372327a44072c7
p0ptartz <ericwm420@gmail.com>
2023-02-21 21:24:00 -0500

    updated destroy error

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,13 +48,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  Category.destroy(req.body, {
+  Category.destroy({
     where: {
       id: req.params.id
     }
-  }).then((data) => {
-    res.json(data)
   })
+    .then((data) => {
+      res.json(data)
+    })
 });
 
 module.exports = router;
