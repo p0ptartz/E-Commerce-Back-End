4825d6756b2857e4d831d86b85b05fc48a65c1bd
p0ptartz <ericwm420@gmail.com>
2023-02-21 20:30:19 -0500

    finished all routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+  Product.findAll({
+    include: [{
+      model: Category
+    }, {
+      model: Tag
+    }]
+  })
+    .then((data) => {
+      res.json(data)
+    })
+})
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [{
+      model: Category
+    }, {
+      model: Tag
+    }]
+  }).then((data) => {
+    res.json(data)
+  })
 });
 
 // create new product
@@ -91,6 +110,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then((data) => {
+    res.json(data)
+  })
 });
 
 module.exports = router;
